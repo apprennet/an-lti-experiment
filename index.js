a067625cf0771a4653f24e0284163230568a5465
Jason Blanchard <jas.blanchard@gmail.com>
2016-03-01 16:32:26 -0500

    added simple LTI endpoint

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import exphbs from 'express-handlebars';
+import lti from 'ims-lti';
 
 var app = express();
 
@@ -19,12 +20,50 @@ app.use(cookieParser('sekret'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.enable('trust proxy');
+
 app.get('/', (req, res) => {
   res.send('working');
 });
 
 app.get('/page', (req, res) => {
   res.render('page', {value: 'test'})
+});
+
+const ltiConsumers = {
+  123: {
+    key: '123',
+    secret: 'sekret'
+  }
+};
+
+app.post('/launch', (req, res) => {
+  let key = req.body.oauth_consumer_key;
+  let consumer = ltiConsumers[key];
+
+  if (!consumer) {
+    return res.status(500).send('consumer not recognized');
+  }
+
+  let provider = new lti.Provider(key, consumer.secret);
+
+  provider.valid_request(req, (err, isValid) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    if (provider.outcome_service) {
+      provider.outcome_service.send_replace_result(.7, (err, result) => {
+        if (err) {
+          console.log(`Error posting result ${err}`);
+        }
+
+        console.log('Success posting result');
+      })
+    }
+
+    return res.json(req.body)
+  });
 });
 
 // 404s
